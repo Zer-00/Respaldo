63c7dc39c6e6b6817e586155f9e6c11f3fff5506
unknown <UFG@CC5-43.ufg.local>
2023-11-04 11:40:42 -0600

    jasnjd

--- src/main/java/com/parcial3/parcial3/Controller/DepartamentosController.java
+++ src/main/java/com/parcial3/parcial3/Controller/DepartamentosController.java
@@ -1,5 +1,27 @@
 package com.parcial3.parcial3.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.parcial3.parcial3.Entities.Departamentos;
+import com.parcial3.parcial3.Repositories.DepartamentosRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/zzz")
+@RequiredArgsConstructor
 public class DepartamentosController {
-    
+    @Autowired
+    private DepartamentosRepository departamentosRepository;
+
+    @GetMapping("/obtener")
+    public List<Departamentos> getDepartamentos(){
+        return departamentosRepository.findAll();
+    }
+
 }
